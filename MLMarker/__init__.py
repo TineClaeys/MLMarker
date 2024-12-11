d6ac5185ca08b9837a2e0953eb9fdede2805707d
TineClaeys <tineclae.claeys@ugent.be>
2024-12-11 13:35:17 +0100

    Refactored code

--- MLMarker/__init__.py
+++
@@ -1,6 +0,0 @@
-from .constants import MODEL_DIR, BINARY_MODEL_PATH, MULTI_CLASS_MODEL_PATH
-from .explainability import Explainability
-from .model import MLMarker
-from .utils import validate_sample
-
-__version__ = "0.1"
